1c208d3f60670bd3c976347756c9549bd6090359
tmtuan <tmtuan.projects@gmail.com>
2016-01-23 13:50:22 +0700

    Initialize pickerApp

--- pickerApp/ViewController.swift
+++ pickerApp/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate {
+    
+    @IBOutlet var itemLabel: UILabel!
+    
+    var food = ["apples", "bananas", "grapes", "pears", "blueberries"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        itemLabel.text = food[0]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+         return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return food.count
+    
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return food[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        var itemSelected = food[row]
+        itemLabel.text = itemSelected
+    }
 }
 
